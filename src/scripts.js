e8cca3f41de56f19f63e1448ea44674177903e4e
Hamzaelhamdani <62265033+Hamzaelhamdani@users.noreply.github.com>
2024-05-15 15:09:04 +0100

    Update scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,15 @@
 const users = [
-    { username: "user1", password: "pass1" },
-    { username: "user2", password: "pass2" },
-    { username: "user3", password: "pass3" },
-    { username: "user4", password: "pass4" },
-    { username: "user5", password: "pass5" },
-    { username: "user6", password: "pass6" },
-    { username: "user7", password: "pass7" },
-    { username: "user8", password: "pass8" },
-    { username: "user9", password: "pass9" },
-    { username: "user10", password: "pass10" },
-    { username: "user11", password: "pass11" }
+    { username: "user1", password: "odcpass1" },
+    { username: "user2", password: "odcpass2" },
+    { username: "user3", password: "odcpass3" },
+    { username: "user4", password: "odcpass4" },
+    { username: "user5", password: "odcpass5" },
+    { username: "user6", password: "odcpass6" },
+    { username: "user7", password: "odcpass7" },
+    { username: "user8", password: "odcpass8" },
+    { username: "user9", password: "odcpass9" },
+    { username: "user10", password: "odcpass10" },
+    { username: "user11", password: "odcpass11" }
 ];
 
 document.getElementById('loginForm').addEventListener('submit', function(e) {
